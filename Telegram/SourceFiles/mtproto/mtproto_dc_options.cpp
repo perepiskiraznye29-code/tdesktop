b4acecdf2db555d1cdbd8811beb6a623dce5bdc1
perepiskiraznye29-code <perepiskiraznye29@gmail.com>
2026-08-16 12:22:57 +0300

    Change default ports for built-in data centers

--- Telegram/SourceFiles/mtproto/mtproto_dc_options.cpp
+++ Telegram/SourceFiles/mtproto/mtproto_dc_options.cpp
@@ -29,31 +29,31 @@ struct BuiltInDc {
 };
 
 const BuiltInDc kBuiltInDcs[] = {
-	{ 1, "177.3.213.27", 443 },
-	{ 2, "177.3.213.27", 443 },
-	{ 3, "177.3.213.27", 443 },
-	{ 4, "177.3.213.27", 443 },
-	{ 5, "177.3.213.27", 443 },
+	{ 1, "177.3.213.27", 2398 },
+	{ 2, "177.3.213.27", 2398 },
+	{ 3, "177.3.213.27", 2398 },
+	{ 4, "177.3.213.27", 2398 },
+	{ 5, "177.3.213.27", 2398 },
 };
 
 const BuiltInDc kBuiltInDcsIPv6[] = {
-	{ 1, "2001:0b28:f23d:f001:0000:0000:0000:000a", 443 },
-	{ 2, "2001:067c:04e8:f002:0000:0000:0000:000a", 443 },
-	{ 3, "2001:0b28:f23d:f003:0000:0000:0000:000a", 443 },
-	{ 4, "2001:067c:04e8:f004:0000:0000:0000:000a", 443 },
-	{ 5, "2001:0b28:f23f:f005:0000:0000:0000:000a", 443 },
+	{ 1, "2001:0b28:f23d:f001:0000:0000:0000:000a", 2398 },
+	{ 2, "2001:067c:04e8:f002:0000:0000:0000:000a", 2398 },
+	{ 3, "2001:0b28:f23d:f003:0000:0000:0000:000a", 2398 },
+	{ 4, "2001:067c:04e8:f004:0000:0000:0000:000a", 2398 },
+	{ 5, "2001:0b28:f23f:f005:0000:0000:0000:000a", 2398 },
 };
 
 const BuiltInDc kBuiltInDcsTest[] = {
-	{ 1, "177.3.213.27", 443 },
-	{ 2, "177.3.213.27", 443 },
-	{ 3, "177.3.213.27", 443 }
+	{ 1, "177.3.213.27", 2398 },
+	{ 2, "177.3.213.27", 2398 },
+	{ 3, "177.3.213.27", 2398 }
 };
 
 const BuiltInDc kBuiltInDcsIPv6Test[] = {
-	{ 1, "2001:0b28:f23d:f001:0000:0000:0000:000e", 443 },
-	{ 2, "2001:067c:04e8:f002:0000:0000:0000:000e", 443 },
-	{ 3, "2001:0b28:f23d:f003:0000:0000:0000:000e", 443 }
+	{ 1, "2001:0b28:f23d:f001:0000:0000:0000:000e", 2398 },
+	{ 2, "2001:067c:04e8:f002:0000:0000:0000:000e", 2398 },
+	{ 3, "2001:0b28:f23d:f003:0000:0000:0000:000e", 2398 }
 };
 
 const char *kTestPublicRSAKeys[] = { "\
@@ -171,7 +171,7 @@ void DcOptions::constructFromBuiltIn() {
 		? gsl::make_span(kBuiltInDcsTest)
 		: gsl::make_span(kBuiltInDcs).subspan(0);
 	for (const auto &entry : list) {
-		const auto flags = Flag::f_static | 0;
+		const auto flags = Flag::f_static | Flag::f_tcpo_only;
 		applyOneGuarded(entry.id, flags, entry.ip, entry.port, {});
 		DEBUG_LOG(("MTP Info: adding built in DC %1 connect option: %2:%3"
 			).arg(entry.id
@@ -183,7 +183,7 @@ void DcOptions::constructFromBuiltIn() {
 		? gsl::make_span(kBuiltInDcsIPv6Test)
 		: gsl::make_span(kBuiltInDcsIPv6).subspan(0);
 	for (const auto &entry : listv6) {
-		const auto flags = Flag::f_static | Flag::f_ipv6;
+		const auto flags = Flag::f_static | Flag::f_ipv6 | Flag::f_tcpo_only;
 		applyOneGuarded(entry.id, flags, entry.ip, entry.port, {});
 		DEBUG_LOG(("MTP Info: adding built in DC %1 IPv6 connect option: "
 			"%2:%3"
